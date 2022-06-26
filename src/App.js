7c8c8815242a347605aec9aeb74f730d7f745120
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-26 14:16:27 +0200

    Issues are resolved and connection with firebase

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector, useDispatch } from "react-redux";
-import useHttp from "./hooks/use-http";
-import { useEffect, Fragment } from "react";
-import { uiActions } from "./store/ui-slice";
-import Notification from "./components/UI/Notification";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
+
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-  const { sendRequest } = useHttp();
-  const dispatch = useDispatch();
-  const dataSent = () => {
-    dispatch(
-      uiActions.showNotification({
-        status: "success",
-        title: "Success",
-        message: "Cart Data has been sent",
-      })
-    );
-  };
+
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending",
-        message: "Sending Cart Data",
-      })
-    );
-    sendRequest(
-      {
-        url: "https://react-http-51a82-default-rtdb.firebaseio.com/cart.json",
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(cart),
-      },
-      dataSent.bind(null)
-    );
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
@@ -55,7 +30,7 @@ function App() {
           status={notification.status}
           title={notification.title}
           message={notification.message}
-        ></Notification>
+        />
       )}
       <Layout>
         {showCart && <Cart />}
